7e6a4685fa1874cf2eec529586246bf4b1e2967b
Johnny Oshika <johnnyoshika@gmail.com>
2020-04-24 23:30:14 -0700

    Set up server in index.ts

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import * as express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});

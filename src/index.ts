80875334a5d62d64ad7f72414ca0e4fad6257388
Johnny Oshika <johnnyoshika@gmail.com>
2020-04-26 09:14:11 -0700

    Route 'point' subdomain to '/point'

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,21 @@ import 'dotenv/config';
 import * as express from 'express';
 const app = express();
 
+app.all('*', (req, res, next) => {
+  if (req.subdomains.length === 1 && req.subdomains[0] === 'point')
+    req.url = `/point${req.url}`;
+
+  next();
+});
+
+app.all('/point/*', (req, res) => {
+  res.json({
+    url: req.url,
+    method: req.method,
+    route: req.params[0],
+  });
+});
+
 app.get('/', (req, res) => {
   res.json({
     message: 'Hi',

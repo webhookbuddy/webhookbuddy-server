f69b58393e1ea0067d86e9b9631f2a3950c918c3
Johnny Oshika <johnnyoshika@gmail.com>
2020-04-28 16:02:25 -0700

    Define schema

---
+++ src/schema/endpoint.ts
@@ -0,0 +1,34 @@
+import { gql } from 'apollo-server-express';
+
+export default gql`
+  extend type Query {
+    endpoint(id: ID!): Endpoint
+    endpoints: [Endpoint!]!
+  }
+
+  extend type Mutation {
+    createEndpoint(input: CreateEndpointInput): Endpoint!
+    deleteEndpoint(input: DeleteEndpointInput): Boolean!
+  }
+
+  type Endpoint {
+    id: ID!
+    createdAt: DateTime!
+    updatedAt: DateTime!
+    referenceId: String!
+    name: String!
+    forwardUrls: [ForwardUrl!]!
+  }
+
+  type ForwardUrl {
+    url: String!
+  }
+
+  input CreateEndpointInput {
+    name: String!
+  }
+
+  input DeleteEndpointInput {
+    id: ID!
+  }
+`;

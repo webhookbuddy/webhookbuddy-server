5116b33ab523655f4e6b1561f39836955dfb94df
Johnny Oshika <johnnyoshika@gmail.com>
2020-04-29 12:39:19 -0700

    Add processWebhook test

---
+++ src/services/processWebhook.spec.ts
@@ -0,0 +1,34 @@
+import 'mocha';
+import { expect } from 'chai';
+import * as proxyquire from 'proxyquire';
+import { QueryConfig } from 'pg';
+
+describe('processWebhook', () => {
+  it('should return newly created webhook', async () => {
+    const processWebhook = proxyquire('./processWebhook', {
+      '../db': {
+        default: {
+          query: (
+            text: string | QueryConfig<any>,
+            params?: Array<any>,
+          ) => ({
+            rows: [
+              {
+                id: 1,
+              },
+            ],
+          }),
+        },
+      },
+    }).default;
+
+    const result = await processWebhook({
+      referenceId: '123',
+      contentType: 'application/json',
+    });
+    expect(result).to.have.property('id');
+    expect(result.id).to.equal(1);
+  });
+
+  // I tried, but could not test the 'throw' scenario
+});

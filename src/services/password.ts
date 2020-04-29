6c0b6c14026d5b4de30794e3d281e47ff45f22e9
Johnny Oshika <johnnyoshika@gmail.com>
2020-04-28 20:08:13 -0700

    Add password service

---
+++ src/services/password.ts
@@ -0,0 +1,22 @@
+import * as crypto from 'crypto';
+
+const iterations = 10000;
+
+const saltedHash = (password: string, salt: string) =>
+  crypto
+    .pbkdf2Sync(password, salt, iterations, 512, 'sha512')
+    .toString('hex');
+
+export const hashPassword = (password: string) => {
+  const salt = crypto.randomBytes(16).toString('hex');
+  return {
+    hash: saltedHash(password, salt),
+    salt,
+  };
+};
+
+export const verifyPassword = (
+  password: string,
+  hash: string,
+  salt: string,
+) => hash === saltedHash(password, salt);

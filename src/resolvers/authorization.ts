39d72ffdbd57c2236b93cacf7d26a832310afe9f
Johnny Oshika <johnnyoshika@gmail.com>
2020-04-29 22:46:14 -0700

    Implement endpoint query

--- src/resolvers/authorization.ts
+++ src/resolvers/authorization.ts
@@ -1,6 +1,25 @@
 import { ForbiddenError } from 'apollo-server';
 import { skip } from 'graphql-resolvers';
+import db from '../db';
 import { User } from '../types';
 
 export const isAuthenticated = (_, __, { me }: { me: User }) =>
   me ? skip : new ForbiddenError('Not authenticated.');
+
+export const isEndpointAllowed = async (_, { id }, { me }) => {
+  const { rows } = await db.query(
+    `
+      SELECT e.id
+      FROM endpoints as e
+        INNER JOIN user_endpoints as ue on ue.endpoint_id = e.id
+      WHERE 
+        e.id = $1
+        AND
+        ue.user_id = $2
+    `,
+    [id, me.id],
+  );
+
+  if (!rows.length) throw new ForbiddenError('Not allowed.');
+  else return skip;
+};
--- src/resolvers/endpoint.ts
+++ src/resolvers/endpoint.ts
@@ -1,1 +1,49 @@
-export default {};
+import db from '../db';
+import { isAuthenticated, isEndpointAllowed } from './authorization';
+import { combineResolvers } from 'graphql-resolvers';
+
+export default {
+  Query: {
+    endpoint: combineResolvers(
+      isAuthenticated,
+      isEndpointAllowed,
+      async (_, { id }) => {
+        const { rows } = await db.query(
+          `
+            SELECT id, created_at, reference_id, name
+            FROM endpoints
+            WHERE id = $1
+          `,
+          [id],
+        );
+
+        return {
+          id: rows[0].id,
+          createdAt: rows[0].created_at,
+          referenceId: rows[0].reference_id,
+          name: rows[0].name,
+        };
+      },
+    ),
+  },
+
+  Mutation: {},
+
+  Endpoint: {
+    forwardUrls: async (endpoint, _, { me }) => {
+      const { rows } = await db.query(
+        `
+          SELECT id, url
+          FROM forward_urls
+          WHERE 
+            endpoint_id = $1
+            AND 
+            user_id = $2
+        `,
+        [endpoint.id, me.id],
+      );
+
+      return rows;
+    },
+  },
+};

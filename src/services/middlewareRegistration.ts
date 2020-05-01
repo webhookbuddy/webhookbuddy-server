26179beb4476bf6f3b482c1c67e1b4b8caea24c8
Johnny Oshika <johnnyoshika@gmail.com>
2020-05-01 09:36:13 -0700

    Format validation error for better single message format

--- src/services/middlewareRegistration.ts
+++ src/services/middlewareRegistration.ts
@@ -8,6 +8,15 @@ import { applyMiddleware } from 'graphql-middleware';
 import { yupMiddleware } from 'graphql-yup-middleware';
 import { ValidationError } from 'yup';
 
+const formatMessage = (errors: ValidationError[]) =>
+  !errors.length
+    ? 'Error encountered.'
+    : errors.length === 1
+    ? `${errors[0].message}.`
+    : `${errors.length} Errors: ${errors
+        .map((e, i) => `${i + 1}) ${e.message}.`)
+        .join(' ')}`;
+
 export const schemaWithMiddleware = (
   typeDefs: ITypeDefinitions,
   resolvers?: IResolvers<any> | Array<IResolvers<any>>,
@@ -19,13 +28,7 @@ export const schemaWithMiddleware = (
     }),
     yupMiddleware({
       errorPayloadBuilder: (error: ValidationError) => {
-        throw new UserInputError(
-          `${error.inner.length} Error${
-            error.inner.length > 1 ? 's' : ''
-          }: ${error.inner
-            .map((e, i) => `${i + 1}) ${e.message}.`)
-            .join(' ')}`,
-        );
+        throw new UserInputError(formatMessage(error.inner));
       },
     }),
   );

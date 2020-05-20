0b5bc2f3bb4954aee3c54ae3e40a9ca73bdb065b
Johnny Oshika <johnnyoshika@gmail.com>
2020-05-20 08:16:28 -0700

    Add description for SendGrid webhooks

--- src/services/getDescription.ts
+++ src/services/getDescription.ts
@@ -17,12 +17,29 @@ const tryGetStripe = (entity: any) =>
     ? `${getStripeCustomer(entity)} ${entity.body_json?.type}`
     : null;
 
+const tryGetSendgrid = (entity: any) =>
+  mapToKeyValue(entity.headers).find(
+    o =>
+      o.key.toLowerCase() === 'user-agent' &&
+      o.value === 'SendGrid Event API',
+  ) && Array.isArray(entity.body_json)
+    ? entity.body_json.length > 5
+      ? `${entity.body_json.length} messages: ${entity.body_json
+          .map(o => o.event)
+          .slice(0, 3)
+          .join(',')}...`
+      : entity.body_json.map(o => o.event).join(',')
+    : null;
+
 const tryGetArray = (entity: any) =>
   Array.isArray(entity.body_json)
     ? `${entity.body_json.length} messages`
     : null;
 
 const getDescription = (entity: any) =>
-  tryGetStripe(entity) ?? tryGetArray(entity) ?? entity.method;
+  tryGetStripe(entity) ??
+  tryGetSendgrid(entity) ??
+  tryGetArray(entity) ??
+  entity.method;
 
 export default getDescription;

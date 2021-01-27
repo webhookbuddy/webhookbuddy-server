a4aca9a9af9e73487604e38f8e7c13e5eab8eafb
Johnny Oshika <johnnyoshika@gmail.com>
2021-01-26 16:40:17 -0800

    Add config

---
+++ src/config/index.ts
@@ -0,0 +1,19 @@
+const config = {
+  port: process.env.PORT || 8000, // Heroku sets process.env.PORT
+  environment: {
+    dev: process.env.NODE_ENV === 'development',
+    prod: process.env.NODE_ENV === 'production',
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
+  database: {
+    url: process.env.DATABASE_URL, // Heroku sets process.env.DATABASE_URL
+
+    name: process.env.DATABASE,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT, 10),
+  },
+};
